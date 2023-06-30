ccdf2c25a7e9e0b3368cc50eba2f8a60dd3b4be6
Mykhailo <eglet777@ukr.net>
2023-06-30 15:11:29 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import {DataService} from "./data.service";
+import {SelectService} from "./select.service";
+import {ResultService} from "./result.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: []
 })
 export class AppComponent {
-  title = 'quiz-app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,39 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { QuizListComponent } from './quiz-list/quiz-list.component';
+import {HttpClientModule} from "@angular/common/http";
+import { QuizItemComponent } from './quiz-list/quiz-item/quiz-item.component';
+import {AppRoutingModule} from "./app-routing.module";
+import { PlayItemComponent } from './play/play-item/play-item.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { ResultsComponent } from './results/results.component';
+import { StatsComponent } from './stats/stats.component';
+import {DataService} from "./data.service";
+import {SelectService} from "./select.service";
+import {ResultService} from "./result.service";
+import {StatsService} from "./stats.service";
+import {NgApexchartsModule} from "ng-apexcharts";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    QuizListComponent,
+    QuizItemComponent,
+    PlayItemComponent,
+    ResultsComponent,
+    StatsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    NgApexchartsModule
   ],
-  providers: [],
+  providers: [DataService,SelectService,ResultService,StatsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
